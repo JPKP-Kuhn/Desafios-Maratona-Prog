ecd05369df50767cc5b37c348a17e77c08b8dded
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2026-05-24 20:00:12 -0300

    distancia entre amigos

---
+++ obi/treino_1805/distancia_entre_amigos.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define _                                                                      \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL);
+#define fr first
+#define sc second
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define LSOne(S) ((S) & -(S)) // bit menos significativo ligado, bitmask
+
+using ll = long long;
+using vll = vector<ll>;
+using vb = vector<bool>;
+using pl = pair<ll, ll>;
+using vpll = vector<pair<ll, ll>>;
+
+// M_PI para número PI
+const ll MAXN = 1e5 + 10; // Range seguro para vetores e arrays
+const ll INF = 1e18 + 5;  // PD e inicialização de valor mínimo
+const ll MOD = 1e9 + 7;   // aritmética modular, números muito grandes
+const double EPS = 1e-9;  // Valor para comparar double
+
+int main() {
+  _ ll n;
+  cin >> n;
+  vll a(n);
+  for (ll &x : a)
+    cin >> x;
+
+  ll best = a[0];
+  ll ans = 0;
+
+  for (ll j = 1; j < n; j++) {
+    ans = max(ans, best + a[j] + j);
+
+    best = max(best, a[j] - j);
+  }
+  cout << ans << '\n';
+  return 0;
+}
