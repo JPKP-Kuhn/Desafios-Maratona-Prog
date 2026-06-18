163095ce5324df38e3be3a2daa38796bb25816ec
JPKP-Kuhn <pedrojoaojpk@gmail.com>
2026-06-17 23:11:09 -0300

    vetores

--- maratonas/pre_seletiva_individual/insane_shot.cpp
+++ maratonas/pre_seletiva_individual/insane_shot.cpp
@@ -11,53 +11,35 @@ using vb = vector<bool>;
 using pl = pair<ll, ll>;
 using vpll = vector<pair<ll, ll>>;
 
-const double EPS = 1e-4; // Valor para comparar double
-
-struct vetor {
-  double x, y;
-
-  double compr() const { return sqrt(x * x + y * y); }
-
-  vetor operator+(const vetor &other) const {
-    return {x + other.x, y + other.y};
-  }
-
-  vetor operator-(const vetor &other) const {
-    return {x - other.x, y - other.y};
-  }
-
-  vetor operator*(double k) const { return {x * k, y * k}; }
-
-  vetor operator/(double other) const { return {x / other, y / other}; }
-};
+const double EPS = 1e-9; // Valor para comparar double
 
 int main() {
-  _ vetor ctank;
-  ll raio;
-  cin >> ctank.x >> ctank.y >> raio;
-  vetor sher;
-  cin >> sher.x >> sher.y;
-  vetor enemy;
-  cin >> enemy.x >> enemy.y;
+  _ ll cx, cy, raio;
+  cin >> cx >> cy >> raio;
 
-  vetor difu = sher - ctank;
-  vetor u = difu / difu.compr();
+  ll sx, sy, ex, ey;
+  cin >> sx >> sy >> ex >> ey;
 
-  vetor difv = enemy - ctank;
-  vetor v = difv / difv.compr();
+  double mx = (double)(ex + sx) / 2.0;
+  double my = (double)(ey + sy) / 2.0;
 
-  vetor soma = u + v;
+  double ux = mx - (double)cx;
+  double uy = my - (double)cy;
+  double tam = sqrt(ux * ux + uy * uy);
 
-  if (soma.compr() < EPS) {
+  if (tam - raio < EPS) {
     cout << "No way\n";
     return 0;
   }
-  vetor n = soma / soma.compr();
 
-  double x1 = ctank.x + n.x * raio;
-  double y1 = ctank.y + n.y * raio;
+  double umx = ux / tam;
+  double umy = uy / tam;
 
-  cout << fixed << setprecision(6) << x1 << ' ' << y1 << '\n';
+  double ansx, ansy;
+  ansx = cx + umx * raio;
+  ansy = cy + umy * raio;
+
+  cout << fixed << setprecision(6) << ansx << ' ' << ansy << '\n';
 
   return 0;
 }
